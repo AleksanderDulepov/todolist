093df2ff281da09e372a4256cbbd1d9a9a4dc994
alexander <sashakb@mail.ru>
2022-12-20 12:10:29 +0300

    added user login tests

--- todolist/settings.py
+++ todolist/settings.py
@@ -127,8 +127,6 @@ SOCIAL_AUTH_VK_OAUTH2_SECRET=os.environ.get("SOCIAL_AUTH_VK_OAUTH2_SECRET")
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
