313f6e93a4b6d1dcae20b77631964e51bf877315
alexander <sashakb@mail.ru>
2022-12-20 20:16:32 +0300

    updated user model

--- core/models.py
+++ core/models.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from core.managers import UserManager, UserRoles
 
 
 class User(AbstractUser):
-
     def __str__(self):
         return self.username
 
